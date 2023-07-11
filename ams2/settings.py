08fe3a42524cae1330a87b34a20fc7a9d919d5cd
mwams101 <85677492+mwams101@users.noreply.github.com>
2023-07-11 17:08:32 +0200

    Update settings.py

--- ams2/settings.py
+++ ams2/settings.py
@@ -130,9 +130,3 @@ STATICFILES_DIRS = (
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-EMAIL_BACKEND =  'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = "mwamba.chitalima@cs.unza.zm"
-EMAIL_HOST_PASSWORD = "iwbtbmfpitw2020"
